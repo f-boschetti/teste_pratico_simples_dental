58a16beba0eb0101f6be9455640b0f463ea2450f
f-boschetti <felipeboss232@gmail.com>
2024-10-13 10:00:11 -0300

    feat: Inicia projeto
    Adiciona estrutura e configuração inicial do projeto para o teste pratico da SimplesDental.

---
+++ src/test/java/com/example/simplesDental/SimplesDentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplesDental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesDentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
