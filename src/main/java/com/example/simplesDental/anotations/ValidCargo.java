42891011cde115518339d476a764d97d38d32cb6
f-boschetti <felipeboss232@gmail.com>
2024-10-13 18:03:20 -0300

    feat: adiciona classes de erro e validação para cargos

---
+++ src/main/java/com/example/simplesDental/anotations/ValidCargo.java
@@ -0,0 +1,21 @@
+package com.example.simplesDental.anotations;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CargoValidator.class)
+@Target({ ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidCargo {
+
+    String message() default "Cargo inválido";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/example/simplesDental/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.simplesDental.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
